fcf3961074d0e923bcc35b03b6820e609fb32212
Milkessa Gebi <milkessagebi@gmail.com>
2021-08-07 12:05:30 +0300

    m

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import { Teacher } from "./teacher";
+import React from "react";
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
 
-const teacher= new Teacher("Milkii","Bsc");
-teacher.teach();
+ReactDOM.render(<Counter/>, document.getElementById('root'));
